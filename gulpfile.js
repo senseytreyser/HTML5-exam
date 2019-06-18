dd7a6e7838a07f1bf821d633a7b928b6395d888b
Senseytreyser <senseytreyser@mail.ru>
2019-06-18 17:43:19 +0300

    Подключение и настройка gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+
+gulp.task('style', function(){
+    return gulp.src('frontend/scss/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(scss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('clean',function(){
+    return del('public');
+});
+
+gulp.task('assets', function(){
+    return gulp.src('frontend/assets/**')
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('style', 'assets')))
